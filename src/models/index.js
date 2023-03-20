14b9d479966dd2839c81b5ed4ef81d8213b9e5d3
Potatodori <jshj5181@naver.com>
2023-03-21 05:13:42 +0900

    알바 후기 및 평가리스트 기능추가

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,8 @@ import { UserController, UserSwagger } from "./users";
 import { TaskController } from "./tasks";
 import { CategoryController } from "./task_categories";
 import { TodoWorkController } from "./todoworks";
+import { EmplReviewController } from "./employee_review";
+import { ReviewChklistController } from "./review_checklist";
 
 export const Controllers = [
   UserController,
@@ -10,6 +12,8 @@ export const Controllers = [
   TaskController,
   CategoryController,
   TodoWorkController,
+  EmplReviewController,
+  ReviewChklistController,
 ];
 export const Swaggers = {
   // UserSwagger,
