f890a64b4d360f798030f5a85202a01185a2fffc
Potatodori <jshj5181@naver.com>
2023-03-20 20:26:02 +0900

    todo list/ 카테고리 API 추가

--- src/models/index.js
+++ src/models/index.js
@@ -2,12 +2,14 @@ import { AuthController } from "./auth";
 import { UserController, UserSwagger } from "./users";
 import { TaskController } from "./tasks";
 import { CategoryController } from "./task_categories";
+import { TodoWorkController } from "./todoworks";
 
 export const Controllers = [
   UserController,
   AuthController,
   TaskController,
   CategoryController,
+  TodoWorkController,
 ];
 export const Swaggers = {
   // UserSwagger,
